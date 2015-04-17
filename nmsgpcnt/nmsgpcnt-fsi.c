922385ec8bfc3a095453c5d0de04cbbf061cad21
Mike Schiffman <mschiffm@fsi.io>
2015-04-17 09:26:27 -0700

    remove NMSG seqsrc processing from nmsgpcnt-fsi

--- nmsgpcnt/nmsgpcnt-fsi.c
+++ nmsgpcnt/nmsgpcnt-fsi.c
@@ -33,18 +33,13 @@
 #define PB_WT_LD            2   /* wire type: length delimited */
 #define PB_WT_32            5   /* wire type: 32-bit */
 
-/* protocol buffer NMSG field numbers */
+/* protocol buffer Nmsg field numbers */
 #define PB_NMSG_PAYLOAD     1   /* payload */
 #define PB_NMSG_PAYLOAD_CRC 2   /* payload CRC */
 #define PB_NMSG_SEQSRC      3   /* sequence source */
 #define PB_NMSG_SEQID       4   /* sequence ID */
 
-/* NMSG container is zlib compressed. */
-#define NMSG_FLAG_ZLIB      0x01
-/* NMSG container is fragmented. */
-#define NMSG_FLAG_FRAGMENT  0x02
-
-/* decode a varint and stick retulsts in val */
+/* decode a varint and stick results in val */
 #define CHNM_DCDE_VARINT(shifter, buf, val, idx)    \
     shifter = 0, val = 0;                           \
     do                                              \
@@ -55,32 +50,17 @@
     }                                               \
     while (buf[idx] & 0x80);                        \
 
-/* copy 16 bits from buf to out, ensure network byte order */
-#define load_net16(buf, out)                        \
-    do                                              \
-    {                                               \
-        uint16_t _my_16;                            \
-        memcpy(&_my_16, buf, sizeof(uint16_t));     \
-        _my_16 = ntohs(_my_16);                     \
-        *(out) = _my_16;                            \
-    }                                               \
-    while (0)
-
-/* holds results of PB deserialization */
-struct nmsg_gpbd_data
+/* holds results of protobuf deserialization */
+struct nmsg_pb_data
 {
     uint32_t n_payloads;    /* number of payloads */
-    bool has_seqsrc;        /* has a seqsrc */
-    uint32_t seqsrc;        /* seqsrc */
-    bool has_seqid;         /* has a seqid */
-    uint64_t seqid;         /* seqid */
 };
-typedef struct nmsg_gpbd_data nmsg_gpbd_data_t;
+typedef struct nmsg_pb_data nmsg_pb_data_t;
 
 int
-des(const uint8_t *pb_data, uint32_t p_len, nmsg_gpbd_data_t *nmsg_data)
+decode_pb(const uint8_t *pb_data, uint32_t p_len, nmsg_pb_data_t *nmsg_data)
 {
-    bool did_seqsrc, did_seqid;
+    int idx;
     uint8_t wire_type;
     uint64_t i, j;
     uint64_t shifter;
@@ -88,19 +68,18 @@ des(const uint8_t *pb_data, uint32_t p_len, nmsg_gpbd_data_t *nmsg_data)
     memset(nmsg_data, 0, sizeof (*nmsg_data));
 
     i = 0;
-    did_seqid = false;
-    did_seqsrc = false;
+    idx = 0;
     while (i < p_len)
     {
         /*
          *  Each key in the streamed message is a varint with the value 
          *  (field_number << 3) | wire_type.
          *  
-         *  To decode a GPB byte, AND off the lower 3 bits
+         *  To decode a protobuf byte, AND off the lower 3 bits
          *  (GPB_WIRETYPE_MASK) to get the wire type. From there, right 
          *  shift by 3 to get the field number.
          *
-         *  For an NMSG GPB, the field numbers are:
+         *  For an NMSG protobuf, the field numbers are:
          *
          *  message Nmsg
          *  {
@@ -110,7 +89,7 @@ des(const uint8_t *pb_data, uint32_t p_len, nmsg_gpbd_data_t *nmsg_data)
          *      optional uint64      sequence_id  = 4;
          *  }
          *
-         *  And the GPB wiretypes are:
+         *  And the protobuf wiretypes are:
          *
          *  0 Varint            PB_WT_VI
          *  1 64-bit            PB_WT_64
@@ -121,29 +100,12 @@ des(const uint8_t *pb_data, uint32_t p_len, nmsg_gpbd_data_t *nmsg_data)
         switch (wire_type)
         {
             case PB_WT_VI:
-                if ((pb_data[i] >> 3) == PB_NMSG_SEQSRC)
-                {
-                    nmsg_data->has_seqsrc = true;
-                }
-                else if ((pb_data[i] >> 3) == PB_NMSG_SEQID)
-                {
-                    nmsg_data->has_seqid = true;
-                }
                 CHNM_DCDE_VARINT(shifter, pb_data, j, i);
-                if (nmsg_data->has_seqsrc == true && did_seqsrc == false)
-                {
-                    nmsg_data->seqsrc = j;
-                    did_seqsrc = true;
-                }
-                if (nmsg_data->has_seqid == true && did_seqid == false)
-                {
-                    nmsg_data->seqid = j;
-                    did_seqid = true;
-                }
                 i++;
                 break;
             case PB_WT_64:
-                i += 8;
+                /* next byte has the 64-bit value; skip both */
+                i += 9;
                 break;
             case PB_WT_LD:
                 if ((pb_data[i] >> 3) == PB_NMSG_PAYLOAD)
